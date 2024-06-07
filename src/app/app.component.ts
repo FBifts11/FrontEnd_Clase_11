c25429a14f80c0191bdf90056cc2bddac16c5edd
FBifts11 <federico.balbuena@bue.edu.ar>
2024-06-07 14:53:59 -0300

    Subo proyecto base para comenzar a trabajar en clase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'productList';
-}
+  title = 'pelis-app';
+
+  private _router = inject(Router)
+  
+  navigateTo(){
+    this._router.navigate(['/contact'])
+  }}
